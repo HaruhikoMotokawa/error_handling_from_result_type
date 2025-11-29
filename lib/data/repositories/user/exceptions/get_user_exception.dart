0bf2c3891ca806b8e7fa8ac8f842f35724aae3ed
本川　晴彦 <harx6009@icloud.com>
2025-11-29 22:36:27 +0900

    feat: GetUserExceptionの構造を改善し、原因を直接保持するように変更

--- lib/data/repositories/user/exceptions/get_user_exception.dart
+++ lib/data/repositories/user/exceptions/get_user_exception.dart
@@ -1,20 +1,20 @@
-import 'package:error_handling_from_result_type/core/exceptions/app_exception.dart';
 import 'package:error_handling_from_result_type/data/repositories/user/exceptions/fetch_user_exception.dart';
 import 'package:error_handling_from_result_type/data/repositories/user/exceptions/save_user_exception.dart';
 
 sealed class GetUserException implements Exception {
-  const GetUserException({required this.cause});
-
-  /// 原因となった例外
-  final AppException cause;
+  const GetUserException();
 }
 
 /// リモート取得中に発生したエラー（中身に FetchUserException を丸ごと持つ）
 class GetUserFetchException extends GetUserException {
-  const GetUserFetchException(FetchUserException cause) : super(cause: cause);
+  const GetUserFetchException(this.cause);
+
+  final FetchUserException cause;
 }
 
 /// ローカル保存中に発生したエラー（中身に SaveUserException を丸ごと持つ）
 class GetUserSaveException extends GetUserException {
-  const GetUserSaveException(SaveUserException cause) : super(cause: cause);
+  const GetUserSaveException(this.cause);
+
+  final SaveUserException cause;
 }
